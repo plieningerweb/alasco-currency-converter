5e1e748281bfb2f2d883b255d71dba4037ea6695
Andreas Plieninger <plieningerweb@gmail.com>
2019-09-23 16:08:29 +0200

    style: make flake8 pass

--- currency_converter/views.py
+++ currency_converter/views.py
@@ -9,30 +9,26 @@ currency_converter = Blueprint('currency_converter', __name__)
 @currency_converter.route('/convert', methods=['GET', 'POST'])
 def convert():
     """convert amount from base to target currency"""
-    form = ConvertForm(request.form)    
+    form = ConvertForm(request.form)
     has_result = False
     target_amount = False
     has_csrf_token = hasattr(form, 'csrf_token')
     if request.method == 'POST':
         if form.validate_on_submit():
             has_result = True
-            
+
             try:
                 CR = CurrencyRates()
                 target_amount = CR.convert(form.base_currency.data,
                                            form.target_currency.data,
                                            form.base_amount.data)
-                #raise Exception('test')
-            
-            except Exception as e:
+            except Exception:
                 # TODO: test with testing framework
-                #print('error')
-                flash('currency conversion API failed', 'warning')    
+                flash('currency conversion API failed', 'warning')
         else:
             flash('Form could not be validated. See below', 'warning')
-                
-            
-    return render_template('convert.html', 
+
+    return render_template('convert.html',
                            form=form,
                            has_result=has_result,
                            target_amount=target_amount,
